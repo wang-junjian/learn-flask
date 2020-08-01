1a8d2f51c115a332b177595be523d16a860a25d8
wang-junjian <wang-junjian@qq.com>
2020-08-01 19:42:58 +0800

    flask_script

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from flask import request
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return F'{user_agent}'
 
 @app.route('/user/<name>')
 def user(name):
     return f'<h1>Hello {name}!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
