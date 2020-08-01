6149a9b3487e3b24ab06687601e4e4b437ac5113
wang-junjian <wang-junjian@qq.com>
2020-08-01 23:14:28 +0800

    template

--- hello.py
+++ hello.py
@@ -1,14 +1,32 @@
+from datetime import datetime
+
 from flask import Flask
 from flask import request
+from flask import render_template
+
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+moment = Moment(app)
+
 manager = Manager(app)
 
-@app.route('/')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+@app.route('/index.html')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return F'{user_agent}'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
