c0c60656de651bcf1e31677259d8bc8b5f29d86d
wang-junjian <wang-junjian@qq.com>
2020-08-03 23:15:58 +0800

    add database sqlite

--- hello.py
+++ hello.py
@@ -1,20 +1,57 @@
+import os
 from datetime import datetime
 
 from flask import Flask
-from flask import request
-from flask import render_template
+from flask import request, render_template, url_for, session, redirect, flash
 
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+class NameForm(FlaskForm):
+    name = StringField("你的名字是？", validators=[DataRequired()])
+    submit = SubmitField('确定')
+
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
-
 moment = Moment(app)
-
 manager = Manager(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -24,9 +61,25 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html'), 500
 
-@app.route('/index.html')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        user = User.query.filter_by(username=name).first()
+        if not user:
+            user = User(username=name)
+            db.session.add(user)
+            db.session.commit()
+
+            session['known'] = False
+            flash('增加了新用户名 %s' % name)
+        else:
+            session['known'] = True
+        session['name'] = name
+        
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
